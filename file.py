2ed932b2bb58f74f588865b936f7c1d974cc4c1f
CodeWithReo <kitbittu77@gmail.com>
2026-05-22 21:28:39 +0530

    Update file.py

--- file.py
+++ file.py
@@ -6,7 +6,7 @@ st.set_page_config(
     page_title="Chatbot",
     page_icon="🤖"
 )
-api_key = st.secrets["GOOGLE_API_KEY"]
+api_key = st.secrets["AIzaSyDkEg4_l-_MSroxptiICcZaA65omsUtAkQ"]
 genai.configure(api_key="API_KEY")
 model=genai.GenerativeModel("models/gemini-2.5-flash")
 
